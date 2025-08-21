7bee20e0ce33632676d8d273ae4a1a1aec50d81d
Ignacio Spiousas <ignaciospiousas@gmail.com>
2025-08-21 19:31:22 -0300

    Semana 4 R

--- Semana4/Semana4.R
+++ Semana4/Semana4.R
@@ -13,17 +13,44 @@ X <- rbinom(n, size = 1, prob = p)     # Bernoulli = Binomial con size=1
 mean(X)                                # ≈ p
 var(X)                                 # ≈ p*(1-p)
 
+#¿Qué pasa si agrandan el tamaño de la muestra?
+
 #Vamos a graficar la función de probabilidad puntual y la función de distribución acumulada
 
-x_vals <- c(-0.1,0,1,1.1)
-f_x <- dbinom(x_vals, size = 1, prob = p)
-barplot(f_x, names.arg = x_vals, ylim = c(0,1), main = "Bernoulli: f(x)")
+x_vals <- c(-2,-1,0,1,2)
 
+#La función dbinom calcula la probabilidad puntual para una distribución binomial 
+# (de la cual la Bernoulli es un caso espacial para size=1)
+f_x <- dbinom(x_vals, size = 1, prob = p)
+
+data_frame(x = x_vals, f_x = f_x) %>%
+  ggplot(aes(x =  x, y = f_x)) +
+  geom_col() +
+  labs(title = "Bernoulli: f(x)") +
+  theme_bw()
+
+# ¿Qué pasa para los valores que no son 0 o 1?
+
+# Agrego dos puntos fuera de los posibles valores
+x_vals <- c(-2, -1,0,1,2)
 F_x <- pbinom(x_vals, size = 1, prob = p)
-plot(x_vals,F_x,, type = "s", xlab = "x", ylab = "F(x)", main = "Bernoulli: F(x)",ylim = c(0,1))
+
+data_frame(x = x_vals, F_x =F_x) %>%
+  ggplot(aes(x =  x)) +
+  geom_point(aes(y = F_x)) +
+  geom_line(aes(y = F_x)) +
+  labs(title = "Bernoulli: f(x) y F(x)") +
+  theme_bw()
+
+data_frame(x = x_vals, f_x = f_x, F_x =F_x) %>%
+  ggplot(aes(x =  x)) +
+  geom_col(aes(y = f_x), fill = "steelblue", alpha = .2) +
+  geom_point(aes(y = F_x)) +
+  geom_line(aes(y = F_x)) +
+  labs(title = "Bernoulli: f(x) y F(x)") +
+  theme_bw()
 
 #1.2. Binomial X ~ Bi(n,p)
-
 set.seed(123)
 n_sims <- 10000 #Tamaño de la simulación
 n_tr <- 10 #Cantidad de experimentos en cada simulación
@@ -36,11 +63,14 @@ var(X)         # ≈ n_tr * p * (1 - p)
 # Función de probabilidad puntual teórica vs. frecuencia relativa
 x <- 0:n_tr
 f_teo <- dbinom(x, size = n_tr, prob = p)
-
+ 
+# Lo vamos a plotear con herramientas de Rbase para no complicarnos
 hist(X, breaks = -0.5:(n_tr + 0.5), freq = FALSE,
      main = "Binomial: histograma vs f(x) teórica", xlab = "x", ylab="f(x)")
-
+# No hace falta que entiendan el código sino lo que está pasando
 points(x, f_teo, pch = 19)
+
+#¿Cómo ven ambas cosas? ¿Qué pasa si cambian el tamaño de la simulación?
 
 #Función de distribución acumulada
 plot(x, pbinom(x, size = n_tr, prob = p), type = "s",
@@ -76,6 +106,14 @@ curve(punif(x, a, b), from = a - 0.2, to = b + 0.2, lwd = 2,
 #2.2 Normal X~N(mu,sigma^2)
 
 set.seed(21)
+# Para 50 samples
+n_samples <- 50
+mu <- 10; sigma <- 2
+X <- rnorm(n_samples, mean = mu, sd = sigma)
+mean(X); sd(X)   # ≈ mu, sigma
+
+set.seed(21)
+# Para 5000 samples
 n_samples <- 5000
 mu <- 10; sigma <- 2
 X <- rnorm(n_samples, mean = mu, sd = sigma)
@@ -87,11 +125,3 @@ curve(dnorm(x, mu, sigma), add = TRUE, lwd = 2)
 curve(pnorm(x, mu, sigma), from = mu - 4*sigma, to = mu + 4*sigma,
       lwd = 2, main = "Normal: F(x)", ylab = "F(x)")
 
-#Podemos convertirla a distribución normal estándar
-Z <- (X-mu)/sigma
-
-hist(Z, breaks = 40, freq = FALSE, main = "Normal estándar: hist + f(x)")
-curve(dnorm(x, 0, 1), add = TRUE, lwd = 2)
-
-curve(pnorm(x, 0, 1), from =- 4, to = 4,
-      lwd = 2, main = "Normal estándar: F(x)", ylab = "F(x)")
